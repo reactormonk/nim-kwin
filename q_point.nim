0c7b8f29793632b544cacb516f919c5b9041518c
Simon Hafner <hafnersimon@gmail.com>
2013-01-17 19:20:28 -0600

    Q* is not exported.

--- q_point.nim
+++
@@ -1,8 +0,0 @@
-type QPoint* = ref object
-proc QPoint*(xpos: int, ypos: int): QPoint {.importcpp: "QPoint"}
-proc isNull*(point: QPoint): bool {.importcpp: "isNull"}
-proc x*(point: QPoint): int {.importcpp: "x"}
-proc y*(point: QPoint): int {.importcpp: "y"}
-proc setX*(point: QPoint, x: int) {.importcpp: "setX"}
-proc setY*(point: QPoint, y: int) {.importcpp: "setY"}
-proc manhattanLength*(point: QPoint): int {.importcpp: "manhattanLength"}
